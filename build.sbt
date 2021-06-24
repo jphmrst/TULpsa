dc4861e4b83291265f7f4a2cb0d5302d51644066
John Maraist <from-github@maraist.org>
2021-06-23 21:33:19 -0500

    Starting Roster class

--- build.sbt
+++ build.sbt
@@ -1,14 +1,13 @@
 val scala3Version = "3.0.0"
-
-lazy val utils = RootProject(file("/home/jm/Lib/Scala/Utils"))
+unmanagedSources / excludeFilter := ".#*"
+Global / onChangedBuildSource := ReloadOnSourceChanges
 
 lazy val root = project
   .in(file("."))
   .settings(
     name := "scala3-simple",
     version := "0.1.0",
-
     scalaVersion := scala3Version,
-
     libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test"
-  ).dependsOn(utils)
+  ).dependsOn(RootProject(file("/home/jm/Lib/Scala/Utils")))
+
--- src/main/scala/Roster.scala
+++ src/main/scala/Roster.scala
@@ -1,10 +1,31 @@
 
 package org.maraist.wtulrosters
 import java.time.LocalDate
+import org.maraist.latex.{LaTeXRenderable, LaTeXdoc}
 
-case class Roster(
+abstract class Roster(
   val startDate: LocalDate,
-  val slots: Array[Spot]
-) {
+  val slots: Array[Spot],
+  val size: Int,
+  val title: String,
+  val groupLead: String,
+  val footer: String,
+  val indexFormatter: Int => String
+) extends LaTeXRenderable {
+  if (slots.size != size) {
+    throw new IllegalArgumentException(
+      "Expected " + size.toString() + " spots, but array has length "
+        + slots.size.toString())
+  }
 
+  def toLaTeX(doc: LaTeXdoc): Unit = {
+    ???
+  }
 }
+
+class PsaRoster(startDate: LocalDate, slots: Array[Spot])
+    extends Roster(startDate, slots, 78,
+      "WTUL 91.5\\textsc{fm} --- PSA roster",
+      "PSA \\#",
+      "Please report typos, expired spots, or other problems with PSAs to \\textsl{wtul-psa@gmail.com}\\,.",
+      (x: Int) => (1 + x).toString())
