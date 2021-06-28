e833be6abad9b5691e80c4075bd785f75e82daf4
John Maraist <from-github@maraist.org>
2021-06-28 10:10:30 -0500

    Add period to groups

--- src/main/scala/Group.scala
+++ src/main/scala/Group.scala
@@ -1,7 +1,31 @@
 
 package org.maraist.wtulrosters
 
-sealed trait Group(val title: String)
+/**
+  * The default period for the cycle of a PSA spot's priority for
+  * long-term scheduling.
+  */
+val DEFAULT_PERIOD: Double = 8.0
+
+/**
+  * The suggested period for the cycle of a PSA spot's priority for
+  * long-term scheduling of seasonal spots.
+  */
+val SEASONAL_PERIOD: Double = DEFAULT_PERIOD / 2.0
+
+/**
+  * The suggested period for the cycle of a PSA spot's priority for
+  * long-term scheduling of less-frequent spots.
+  */
+val LONG_PERIOD: Double = 2.0 * DEFAULT_PERIOD
+
+/**
+  * The suggested period for the cycle of a PSA spot's priority for
+  * long-term scheduling of rare spots.
+  */
+val RARE_PERIOD: Double = 3.0 * DEFAULT_PERIOD
+
+sealed trait Group(val title: String, val period: Double = DEFAULT_PERIOD)
 
 object Group {
   object Volunteer extends Group("Volunteer opportunities")
@@ -12,14 +36,14 @@ object Group {
   object Mental extends Group("Mental health services")
   object Civic extends Group("Civic opportunities and notices")
   object Animal extends Group("Animal and pet care")
-  object Museum extends Group("Local museums")
-  object Rare extends Group("Rare announcements")
-  object Taxtime extends Group("Tax season announcements")
-  object TaxAlways extends Group("Off-season tax announcements")
-  object StormPrep extends Group("Hurricane preparation")
-  object Holiday extends Group("Holidays")
-  object Carnival extends Group("Carnival seasonal announcements")
-  object Summer extends Group("Summer seasonal announcements")
-  object Voter extends Group("Voter information announcements")
+  object Museum extends Group("Local museums", LONG_PERIOD)
+  object Rare extends Group("Rare", RARE_PERIOD)
+  object Taxtime extends Group("Tax season", SEASONAL_PERIOD)
+  object TaxAlways extends Group("Off-season tax", LONG_PERIOD)
+  object StormPrep extends Group("Hurricane preparation", SEASONAL_PERIOD)
+  object Holiday extends Group("Winter holiday seasonal", SEASONAL_PERIOD)
+  object Carnival extends Group("Carnival season", SEASONAL_PERIOD)
+  object Summer extends Group("Summer season", SEASONAL_PERIOD)
+  object Voter extends Group("Voter information")
 }
 
