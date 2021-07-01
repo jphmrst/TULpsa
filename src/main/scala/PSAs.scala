c4053d3f3603766db1574b917fe69e7c1ab29f7d
John Maraist <from-github@maraist.org>
2021-07-01 10:54:51 -0500

    File headers

--- src/main/scala/PSAs.scala
+++ src/main/scala/PSAs.scala
@@ -1,3 +1,9 @@
+// PSAs.scala --- (c) 2021 John Maraist
+// Part of the WTUL Roster Generator
+//
+// This file is made available under the GNU GPL version 3; see the
+// LICENSE file in this distribution or https://fsf.org/ for more
+// details.
 
 package org.maraist.wtulrosters
 import java.time.LocalDate
